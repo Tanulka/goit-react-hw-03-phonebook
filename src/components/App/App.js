bfdd2e5766edfff0a2ef033ab1e1f03decd00e52
tanulka <tanulkako@gmail.com>
2021-11-19 11:05:03 +0200

    add array in localStorage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -50,6 +50,20 @@ class App extends Component {
     this.setState(prevState => ({ contacts: prevState.contacts.filter(contact => contact.id !== id) }));
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parseContacts = JSON.parse(contacts);
+    if (parseContacts) {
+      this.setState({ contacts: parseContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const { filter, contacts } = this.state;
 
